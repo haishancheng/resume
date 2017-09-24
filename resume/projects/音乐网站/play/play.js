1262fed080494df3db6d5d7c42ee318382312c13
wangtao <1621829316@qq.com>
2017-09-24 22:03:10 +0800

    play模块页面初步完成

---
+++ resume/projects/音乐网站/play/play.js
@@ -0,0 +1,10 @@
+function $(val){
+  return document.querySelector(val)
+}
+function $$(val){
+  return document.querySelectorAll(val)
+}
+
+var audioObject = $('#music')
+// audioObject.play()
+// var audioObject = new Audio('./小幸运.mp3')
